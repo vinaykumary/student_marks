ff9e3600f238c6eeb303440f1f6734e7783a0492
Steve Robinson <stevanity.hf@gmail.com>
2012-07-18 01:02:57 +0530

    Populated Departments

--- db/migrate/20120717192214_populate_departments.rb
+++ db/migrate/20120717192214_populate_departments.rb
@@ -1,9 +1,9 @@
 class PopulateDepartments < ActiveRecord::Migration
   def up
-    Department.create :dept_code=>104,:name=>CSE
-    Department.create :dept_code=>105,:name=>EEE
-    Department.create :dept_code=>106,:name=>ECE
-    Department.create :dept_code=>205,:name=>IT
+    Department.create :dept_code=>104,:name=>"CSE"
+    Department.create :dept_code=>105,:name=>"EEE"
+    Department.create :dept_code=>106,:name=>"ECE"
+    Department.create :dept_code=>205,:name=>"IT"
   end
 
   def down
